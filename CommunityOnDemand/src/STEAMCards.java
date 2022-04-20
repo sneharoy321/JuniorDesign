5d9f5ef246d31e7627a0e76b9197f6b5a0fec5d5
shenderson62 <shendersongt26@gmail.com>
2022-04-20 02:22:07 -0400

    Add hover effects and progress bar

--- CommunityOnDemand/src/STEAMCards.java
+++ CommunityOnDemand/src/STEAMCards.java
@@ -1,9 +1,11 @@
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Class containing the STEAM achievement domain cards.
+ */
 public class STEAMCards {
     public static ArrayList<ImageView> steam = new ArrayList<ImageView>(Arrays.asList(
             new ImageView(new Image("./CropSTEAM/agriculture.png")),
@@ -13,5 +15,15 @@ public class STEAMCards {
             new ImageView(new Image("./CropSTEAM/film.png")),
             new ImageView(new Image("./CropSTEAM/finance.png")),
             new ImageView(new Image("./CropSTEAM/government.png")),
-            new ImageView(new Image("./CropSTEAM/green.png"))));
+            new ImageView(new Image("./CropSTEAM/green.png")),
+            new ImageView(new Image("./CropSTEAM/health.png")),
+            new ImageView(new Image("./CropSTEAM/hospitality.png")),
+            new ImageView(new Image("./CropSTEAM/human.png")),
+            new ImageView(new Image("./CropSTEAM/infotech.png")),
+            new ImageView(new Image("./CropSTEAM/law.png")),
+            new ImageView(new Image("./CropSTEAM/logistics.png")),
+            new ImageView(new Image("./CropSTEAM/manufacturing.png")),
+            new ImageView(new Image("./CropSTEAM/marketing.png")),
+            new ImageView(new Image("./CropSTEAM/steam.png"))));
+
 }
